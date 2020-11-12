d4092dbca5b2f51953f8d535aa402e56bf6fd60e
vika-astrea <vicferdez10@gmail.com>
2020-11-12 15:07:11 -0300

    tokenIsValid

--- tea_market/market_back/routes/userRouter.js
+++ tea_market/market_back/routes/userRouter.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
+const auth = require("../middleware/auth");
 const User = require("../models/userModel");
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
 router.post("/register", async (req, res) => {
   try {
@@ -60,6 +61,33 @@ router.post("/login", async (req, res) =>{
   });
 
   }catch(err){res.status(500).json({error: error.message});}
+})
+
+router.delete("/deleteUser",auth, async(req,res)=>{
+  try{
+    const deletedUser = await User.findByIdAndDelete(req.user);
+    res.json(deletedUser);
+  }catch(err){
+    res.status(500).json({error: err.message})
+  }
+
+})
+
+router.post("/tokenIsValid", async (req,res)=>{
+  try{
+    const token = req.header("x-auth-token");
+    if(!token) return res.json(false);
+    
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    if(!verified) return res.json(false);
+    
+    const user = await User.findById(verified.id);
+    if(!user) return res.json(false);
+
+    return res.json(true)
+  }catch(err){
+    res.status(500).json({error: err.message})
+  }
 })
 
 module.exports = router;

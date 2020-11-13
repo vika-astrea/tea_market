b4e77a0dd5983cbecc53b78a5f21da754302c29f
vika-astrea <vicferdez10@gmail.com>
2020-11-13 15:57:18 -0300

    context

--- tea_market/market_back/routes/userRouter.js
+++ tea_market/market_back/routes/userRouter.js
@@ -90,4 +90,10 @@ router.post("/tokenIsValid", async (req,res)=>{
   }
 })
 
+router.get("/",auth,async(req,res)=>{
+  const user= await User.findById(req.user);
+  res.json({displayName: user.displayName,
+  id:user._id})
+})
+
 module.exports = router;
--- tea_market/market_front/src/App.js
+++ tea_market/market_front/src/App.js
@@ -1,11 +1,43 @@
 import "./App.css";
 import ClippedDrawer from "./components/ClippedDrawer";
+import UserContext from "./context/UserContext";
+import Axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [userData, setUserData] = useState({
+    token: undefined,
+    user: undefined,
+  });
+
+  useEffect(() => {
+    const checkLoggedIn = async () => {
+      let token = localStorage.getItem("auth-token");
+      if (token === null) {
+        localStorage.setItem("auth-token", "");
+        token = "";
+      }
+      const tokenRes = await Axios.post(
+        "http://localhost:5000/user/tokenIsValid",
+        null,
+        { headers: { "x-auth-token": token } }
+      );
+      if (tokenRes.data) {
+        const userRes = await Axios.get("http://localhost:5000/user/", {
+          headers: { "x-auth-token": token },
+        });
+        setUserData({ token, user: userRes.data });
+      }
+    };
+    checkLoggedIn();
+  }, []);
+
   return (
-    <div className="App">
-      <ClippedDrawer />
-    </div>
+    <UserContext.Provider value={{ userData, setUserData }}>
+      <div className="App">
+        <ClippedDrawer />
+      </div>
+    </UserContext.Provider>
   );
 }
 

8e7418f157754b063c997abcfb4dd2159a0d2806
vika-astrea <vicferdez10@gmail.com>
2020-11-23 17:49:42 -0300

    dash at 50%

--- tea_market/market_front/src/Queries.js
+++ tea_market/market_front/src/Queries.js
@@ -4,3 +4,12 @@ export const GetAllProducts= async ()=>{
   const{data}= await axios.get("http://localhost:5000/products/all");
   return data
 }
+
+export const GetUserProducts= async (key)=>{
+  const{data}= await axios({
+    method: "get",
+    url: "http://localhost:5000/products/userProducts",
+    headers: { "X-auth-token": key },
+  });
+  return data
+}
--- tea_market/market_front/src/containers/DashboardContainer.js
+++ tea_market/market_front/src/containers/DashboardContainer.js
@@ -1,24 +1,88 @@
-import { Typography } from '@material-ui/core';
-import React, { useContext} from 'react';
-import NotLogged from '../components/NotLogged';
-import UserContext from '../context/UserContext'
+import {
+  Avatar,
+  IconButton,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemSecondaryAction,
+  ListItemText,
+  Typography,
+} from "@material-ui/core";
+import React, { useContext, useState } from "react";
+import NotLogged from "../components/NotLogged";
+import UserContext from "../context/UserContext";
+import LabelIcon from "@material-ui/icons/Label";
+import DeleteIcon from "@material-ui/icons/Delete";
+import EditIcon from "@material-ui/icons/Edit";
+import { makeStyles } from "@material-ui/core/styles";
+import { useQuery } from "react-query";
+import { GetUserProducts } from "../Queries";
 
-export default function DashboardContainer () {
+
+export default function DashboardContainer() {
   const { userData } = useContext(UserContext);
 
-    return (
-      <>
-        {userData.user ? (<> <Typography variant="h3">
-          My Dashboard
-          
-          
+  const [dense] = useState(false);
+  const [secondary] = useState(false);
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+      maxWidth: 752,
+    },
+    demo: {
+      backgroundColor: theme.palette.background.paper,
+    },
+    title: {
+      margin: theme.spacing(4, 0, 2),
+    },
+  }));
+  const classes = useStyles();
+
+  const { isLoading, error, data } = useQuery(userData.token , GetUserProducts);
+
+  if (isLoading) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
+
+  return (
+    <>
+      {userData.user ? (
+        <>
+          {" "}
+          <Typography variant="h3">My Dashboard</Typography>
+          <Typography variant="h6" className={classes.title}>
+            My items on sale:
           </Typography>
-          Productos en venta 
-          ventas totales
-          ((gráficos ?? ))
-          
-           </>):(<NotLogged/>)}
-      </>
-    )
-  
+          <div className={classes.demo}>
+            <List dense={dense}>
+              {data.map((product)=>{return( <ListItem>
+                  <ListItemAvatar>
+                    <Avatar>
+                      <LabelIcon />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary={product.name}
+                    secondary={secondary ? "Secondary text" : null}
+                  />
+                  <ListItemSecondaryAction>
+                    <IconButton edge="end" aria-label="delete">
+                      <DeleteIcon />
+                    </IconButton>
+                    <IconButton edge="end" aria-label="edit">
+                      <EditIcon />
+                    </IconButton>
+                  </ListItemSecondaryAction>
+                </ListItem>)})}
+               
+              
+            </List>
+          </div>
+        </>
+      ) : (
+        <NotLogged />
+      )}
+    </>
+  );
 }
